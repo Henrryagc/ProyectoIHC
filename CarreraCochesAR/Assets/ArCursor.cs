5fb87ad60a68894b56643c8d8f48c3f37bd1b613
Henrry Gutierrez <henrryagc12@gmail.com>
2021-10-11 20:38:40 -0500

    Configuración y modelo de realidad aumentada

---
+++ CarreraCochesAR/Assets/ArCursor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ArCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cursorChildObject;
+    public GameObject objectToPlace;
+    public ARRaycastManager raycastmanager;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
